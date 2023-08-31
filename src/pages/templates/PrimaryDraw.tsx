0f8241a328ef2a66e878ad27cd55e431c6dad0eb
Apeksha <apekshashetti1012@gmail.com>
2023-08-31 18:52:54 +0530

    Primary and Secondary component complete

--- src/pages/templates/PrimaryDraw.tsx
+++ src/pages/templates/PrimaryDraw.tsx
@@ -1,15 +1,57 @@
-import { Drawer, Box, useMediaQuery, Typography } from "@mui/material";
+import { Box, useMediaQuery, Typography, styled } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useTheme } from "@mui/material/styles";
+import DrawToggle from "../../components/PrimaryDraw/DrawToggle";
+import MuiDrawer from "@mui/material/Drawer";
 
 const PrimaryDraw = () => {
   const theme = useTheme();
   const below600 = useMediaQuery("(max-width:599px)");
   const [open, setOpen] = useState(!below600);
 
+  const openedMixin = () => ({
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: "hidden",
+  });
+  const closedMixin = () => ({
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.primaryDraw.closed,
+  });
+
+  const Drawer = styled(
+    MuiDrawer,
+    {}
+  )(({ theme, open }) => ({
+    width: theme.primaryDraw.width,
+    whiteSpace: "nowrap",
+    boxSizing: "border-box",
+    ...(open && {
+      ...openedMixin(),
+      "& .MuiDrawer-paper": openedMixin(),
+    }),
+    ...(!open && {
+      ...openedMixin(),
+      "& .MuiDrawer-paper": closedMixin(),
+    }),
+  }));
+
   useEffect(() => {
     setOpen(!below600);
   }, [below600]);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Drawer
@@ -17,14 +59,28 @@ const PrimaryDraw = () => {
       variant={below600 ? "temporary" : "permanent"}
       PaperProps={{
         sx: {
-          mt: theme.primaryAppBar.height,
-          height: `calc(100vh - ${theme.primaryAppBar.height} )`,
+          mt: `${theme.primaryAppBar.height}px`,
+          height: `calc(100vh - ${theme.primaryAppBar.height}px)`,
+          width: theme.primaryDraw.width,
         },
       }}
     >
       <Box>
-        <Box>
-          {[...Array(100)].map((_, i) => (
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            p: 0,
+            width: open ? "auto" : "100%",
+          }}
+        >
+          <DrawToggle
+            open={open}
+            handleDrawerClose={handleDrawerClose}
+            handleDrawerOpen={handleDrawerOpen}
+          />
+          {[...Array(50)].map((_, i) => (
             <Typography key={i} paragraph>
               {i + 1}
             </Typography>
