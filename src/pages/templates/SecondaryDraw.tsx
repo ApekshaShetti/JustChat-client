a5efc57a99aa749c2185e76003f68f8dc87f1e56
Apeksha <apekshashetti1012@gmail.com>
2023-09-01 16:20:39 +0530

    global configuration

--- src/pages/templates/SecondaryDraw.tsx
+++ src/pages/templates/SecondaryDraw.tsx
@@ -1,8 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import axios from "axios";
 
 const SecondaryDraw = () => {
   const theme = useTheme();
+  axios
+    .get("http://localhost:8000/api/server/select/?category=cat1")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <Box
       sx={{
